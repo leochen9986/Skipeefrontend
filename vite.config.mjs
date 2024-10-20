ce5f38a5d3f85cf240297b16dd1923bcea6878ca
leochen9986 <92980109+leochen9986@users.noreply.github.com>
2024-10-20 22:49:55 +0800

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -48,6 +48,7 @@ export default defineConfig(({ mode }) => {
       extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.scss'],
     },
     server: {
+      host: "0.0.0.0",
       port: 3000,
       proxy: {
         // https://vitejs.dev/config/server-options.html
